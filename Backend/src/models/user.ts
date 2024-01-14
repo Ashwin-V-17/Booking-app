926d2f1d8792869e28e5c386fe0dc61aad4d2897
Ashwin-V-17 <ashwinv1707@gmail.com>
2024-01-14 22:26:53 +0530

    password encryption and jwt creation

--- Backend/src/models/user.ts
+++ Backend/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 export type UserType={
     _id:string;
     email:string;
@@ -12,6 +13,13 @@ const userSchema=new mongoose.Schema({
         password:{type:String,required:true},
         firstname:{type:String,required:true},
         lastname:{type:String,required:true},
+})
+userSchema.pre("save",async function(next){
+    if(this.isModified('password'))
+    {
+        this.password=await bcrypt.hash(this.password,8);
+        next();
+    }
 })
 const User=mongoose.model<UserType>("User",userSchema);
 export default User;
